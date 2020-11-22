3a52b588ce20bcd72a18ed676d32fd8cf2efb729
verno3632 <verno3632@pixiv.com>
2020-11-23 02:05:52 +0900

    localstorageに保存されるように

--- pages/index.vue
+++ pages/index.vue
@@ -28,6 +28,25 @@ export default {
     Logo,
     VuetifyLogo,
   },
+  mounted() {
+    if (!localStorage.getItem("selectedCards")) {
+      return;
+    }
+
+    try {
+      const selectedCards = JSON.parse(localStorage.getItem("selectedCards"));
+      this.selected = this.cards.filter((card) => {
+        return selectedCards.includes(card.name);
+      });
+    } catch (e) {}
+  },
+  watch: {
+    selected: function (newSelected, oldSelected) {
+      const selectedCards = newSelected.map((card) => card.name);
+      const parsed = JSON.stringify(selectedCards);
+      localStorage.setItem("selectedCards", parsed);
+    },
+  },
   data() {
     return {
       singleSelect: false,
@@ -45,33 +64,21 @@ export default {
       cards: [
         {
           rarity: "SSR",
-          name: "???",
-          idol: "櫻木真乃",
-          unit: "イルミネーションスターズ"
-        },
-        {
-          rarity: "SSR",
           name: "ほわっとスマイル",
           idol: "櫻木真乃",
-          unit: "イルミネーションスターズ"
-        },
-        {
-          rarity: "SSR",
-          name: "???",
-          idol: "櫻木真乃",
-          unit: "イルミネーションスターズ"
+          unit: "イルミネーションスターズ",
         },
         {
           rarity: "SSR",
           name: "はじけてスマイル",
           idol: "櫻木真乃",
-          unit: "イルミネーションスターズ"
+          unit: "イルミネーションスターズ",
         },
         {
           rarity: "SR",
           name: "ばってん長崎恋岬",
           idol: "月岡恋鐘",
-          unit: "アンティーカ"
+          unit: "アンティーカ",
         },
       ],
     };
