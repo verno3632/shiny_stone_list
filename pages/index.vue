4948100f43eac3bc623c7985d45d519fac4f8de4
verno3632 <verno3632@pixiv.com>
2020-11-23 16:34:23 +0900

    大きさを変える

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12" sm="8" md="10">
       <v-data-table
         v-model="selected"
         :headers="headers"
